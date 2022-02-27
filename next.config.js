ae9d78db75dc29b61f0666f0a5d43a760cd51dc9
Michael Grigoryan <56165400+michaelgrigoryan25@users.noreply.github.com>
2022-02-27 15:01:09 +0400

    Minor configuration patch

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 const withPWA = require("next-pwa");
 
-/** @type {import('next').NextConfig} */
 const nextConfig = withPWA({
   pwa: {
-    dest: "public",
     register: true,
     skipWaiting: true,
     disable: process.env.NODE_ENV === "development",
